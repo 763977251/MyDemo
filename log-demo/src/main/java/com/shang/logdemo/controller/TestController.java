d405067fdffb49b77f7644442ff1e894e1953425
尚高 <shanggao@boe.com.cn>
2021-11-01 17:03:18 +0800

    增加测试接口

--- log-demo/src/main/java/com/shang/logdemo/controller/TestController.java
+++ log-demo/src/main/java/com/shang/logdemo/controller/TestController.java
@@ -1,8 +1,10 @@
 package com.shang.logdemo.controller;
 
+import com.shang.logdemo.entity.TestEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +25,17 @@ public class TestController {
     public String test2(@RequestParam String name){
         LOGGER.info("/test2 {}",name);
         return name;
+    }
+
+    @GetMapping("/test3")
+    public TestEntity test3(TestEntity testEntity){
+        LOGGER.info("/test3 {}",testEntity);
+        return testEntity;
+    }
+
+    @GetMapping("/test4")
+    public TestEntity test4(@RequestBody TestEntity testEntity){
+        LOGGER.info("/test4 {}",testEntity);
+        return testEntity;
     }
 }
---
+++ log-demo/src/main/java/com/shang/logdemo/entity/TestEntity.java
@@ -0,0 +1,9 @@
+package com.shang.logdemo.entity;
+
+import lombok.Data;
+
+@Data
+public class TestEntity {
+    String name;
+    String password;
+}
