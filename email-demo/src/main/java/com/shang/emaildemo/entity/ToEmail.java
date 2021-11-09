51bf11e3ce545211cb0905ff1fa50a1f17f440ec
尚高 <shanggao@boe.com.cn>
2021-11-09 09:03:22 +0800

    email-demo

---
+++ email-demo/src/main/java/com/shang/emaildemo/entity/ToEmail.java
@@ -0,0 +1,25 @@
+package com.shang.emaildemo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ToEmail implements Serializable {
+    /**
+     * 邮件接收方，可多人
+     */
+    private String[] tos;
+    /**
+     * 邮件主题
+     */
+    private String subject;
+    /**
+     * 邮件内容
+     */
+    private String content;
+}
