a145102c92c8882b27e3192f27c1853776cff7be
尚高 <shanggao@boe.com.cn>
2021-12-29 15:11:39 +0800

    定时任务锁

--- scheduled_demo/src/main/resources/sql/20211216111358.sql
+++ scheduled_demo/src/main/resources/sql/20211216111358.sql
@@ -5,7 +5,7 @@ Backup Time: 2021-12-16 11:13:58
 */
 
 SET FOREIGN_KEY_CHECKS=0;
-DROP TABLE IF EXISTS `scheduled_demo`.`shedlock`;
+DROP TABLE IF EXISTS `shedlock`;
 CREATE TABLE `shedlock` (
   `name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `lock_until` timestamp(3) NULL DEFAULT NULL,
@@ -14,7 +14,7 @@ CREATE TABLE `shedlock` (
   PRIMARY KEY (`name`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci ROW_FORMAT=DYNAMIC COMMENT='shedlock分布式定时任务表';
 BEGIN;
-LOCK TABLES `scheduled_demo`.`shedlock` WRITE;
-DELETE FROM `scheduled_demo`.`shedlock`;
+LOCK TABLES `shedlock` WRITE;
+DELETE FROM `shedlock`;
 UNLOCK TABLES;
 COMMIT;
