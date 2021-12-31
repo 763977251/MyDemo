b62eb5cda7f213f08a0ba6c5bef482ba1bcace76
尚高 <shanggao@boe.com.cn>
2021-12-31 13:07:52 +0800

    springcloudgateway

---
+++ spring-cloud-gateway-demo/src/main/java/com/shang/springcloudgatewaydemo/SpringCloudGatewayDemoApplication.java
@@ -0,0 +1,13 @@
+package com.shang.springcloudgatewaydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudGatewayDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudGatewayDemoApplication.class, args);
+    }
+
+}
