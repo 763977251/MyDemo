11b8c4216542a45ecab3fb72671316e39c9d6c01
尚高 <shanggao@boe.com.cn>
2022-01-24 09:50:09 +0800

    test

--- demo/src/test/java/com/shang/demo/StrTest/Test.java
+++ demo/src/test/java/com/shang/demo/StrTest/Test.java
@@ -11,4 +11,13 @@ public class Test {
         Matcher m = p.matcher(name);
         System.out.println(m.matches());
     }
+
+    @org.junit.jupiter.api.Test
+    public void test1(){
+        String visitePlace = "字符串字符串字符串字符串字符串字符串1" + "(" + "字符串字符串" + ")";
+        if (visitePlace.length()>20){
+            visitePlace = visitePlace.substring(0,visitePlace.lastIndexOf("("));
+        }
+        System.out.println(visitePlace);
+    }
 }
