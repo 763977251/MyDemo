d44158d5e1d03dac926385e752eb45933f7b0deb
shang <shanggao@boe.com.cn>
2022-05-25 14:08:57 +0800

    websocketClient测试

---
+++ websocket-client/src/main/java/com/shang/websocketclient/MyWebSocketClient.java
@@ -0,0 +1,82 @@
+package com.shang.websocketclient;
+
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONUtil;
+import com.alibaba.fastjson.JSON;
+import lombok.SneakyThrows;
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.handshake.ServerHandshake;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.concurrent.CompletableFuture;
+
+@Service
+public class MyWebSocketClient extends WebSocketClient {
+
+    private static Logger LOG = LoggerFactory.getLogger(MyWebSocketClient.class);
+    private static URI socketUrl;
+
+    static {
+        try {
+            socketUrl = new URI("ws://127.0.0.1:9009/ws/123");
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public MyWebSocketClient() {
+        super(socketUrl);
+    }
+
+    @PostConstruct
+    public void init() throws URISyntaxException {
+        LOG.info("进入init方法");
+        this.connect();
+        CompletableFuture.runAsync(() -> {
+            try {
+                while (true) {
+                    Thread.sleep(10000);
+                    if (!this.isOpen()) {
+                        LOG.info("init--reconnect");
+                        this.reconnect();
+                    }
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        });
+    }
+
+    @Override
+    public void onOpen(ServerHandshake arg0) {
+        // TODO Auto-generated method stub
+        LOG.info("------ MyWebSocket onOpen ------");
+    }
+
+    @Override
+    public void onClose(int arg0, String arg1, boolean arg2) {
+        // TODO Auto-generated method stub
+        LOG.info("------ MyWebSocket onClose ------");
+    }
+
+    @Override
+    public void onError(Exception arg0) {
+        // TODO Auto-generated method stub
+        LOG.info("------ MyWebSocket onError ------");
+    }
+
+    @SneakyThrows
+    @Override
+    public void onMessage(String arg0) {
+        String text = String.valueOf(JSON.parse(arg0));
+        JSONObject jsonObject = JSONUtil.parseObj(text);
+        LOG.info("接收到数据： " + jsonObject);
+    }
+
+}
---
+++ websocket-client/src/test/java/com/shang/websocketclient/WebsocketClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shang.websocketclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
