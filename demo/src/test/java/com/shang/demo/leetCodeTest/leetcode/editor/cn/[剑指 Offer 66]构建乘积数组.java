24dc03f8bf2e4b86d4211cc02e2f59baec1e7b66
shang <shanggao@boe.com.cn>
2023-07-24 11:01:29 +0800

    leetcode + 3

---
+++ demo/src/test/java/com/shang/demo/leetCodeTest/leetcode/editor/cn/[剑指 Offer 66]构建乘积数组.java
@@ -0,0 +1,44 @@
+package com.shang.demo.leetCodeTest.leetcode.editor.cn;//给定一个数组 A[0,1,…,n-1]，请构建一个数组 B[0,1,…,n-1]，其中 B[i] 的值是数组 A 中除了下标 i 以外的元素的积, 即 B[
+//i]=A[0]×A[1]×…×A[i-1]×A[i+1]×…×A[n-1]。不能使用除法。 
+//
+// 
+//
+// 示例: 
+//
+// 
+//输入: [1,2,3,4,5]
+//输出: [120,60,40,30,24] 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 所有元素乘积之和不会溢出 32 位整数 
+// a.length <= 100000 
+// 
+//
+// Related Topics 数组 前缀和 👍 329 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class SolutionOffer66 {
+    public int[] constructArr(int[] a) {
+        int len = a.length;
+        if (len == 0) {
+            return new int[0];
+        }
+        int[] b = new int[len];
+        b[0] = 1;
+        int tmp = 1;
+        for (int i = 1; i < len; i++) {
+            b[i] = b[i - 1] * a[i - 1];
+        }
+        for (int i = len - 2; i >= 0; i--) {
+            tmp *= a[i + 1];
+            b[i] *= tmp;
+        }
+        return b;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
