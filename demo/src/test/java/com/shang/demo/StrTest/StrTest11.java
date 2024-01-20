c199b50b5b90a5c6cfa28a793e9249e8c99136f8
shang <shanggao@boe.com.cn>
2024-01-20 09:06:57 +0800

    test demo

--- demo/src/test/java/com/shang/demo/StrTest/StrTest11.java
+++ demo/src/test/java/com/shang/demo/StrTest/StrTest11.java
@@ -3,6 +3,8 @@ package com.shang.demo.StrTest;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.List;
 
@@ -30,6 +32,15 @@ public class StrTest11 {
         String orgPathId = "1/1179679/9760774/767197196/97588266/961000953";
         System.out.println(Arrays.asList(orgPathId.split("/")));
     }
+
+
+    @Test
+    public void test22(){
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyyMMdd");
+        LocalDate parse = LocalDate.parse(null, dtf);
+        System.out.println(parse.toString());
+    }
+
 
     @Test
     public void test2(){
